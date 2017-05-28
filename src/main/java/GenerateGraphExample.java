3b0166935162125c8e81a4e69956b32a4ffcf9cf
Yuri Bittencourt <yuribitten@gmail.com>
2017-05-28 14:25:20 -0300

    comentários

--- src/main/java/GenerateGraphExample.java
+++ src/main/java/GenerateGraphExample.java
@@ -10,13 +10,21 @@ import org.graphast.model.GraphBounds;
 public class GenerateGraphExample {
 
     public GraphBounds monacoExample() {
+    	
+    	//pegando o caminho para o mapa (.osm.pbf)
         String osmFile = this.getClass().getResource("/monaco-150112.osm.pbf").getPath();
+        //apontando o diretório de armazenamento do grafo
         String graphastMonacoDir = Configuration.USER_HOME + "/graphast/test/monaco";
 
+        /*a clase OSMImporter é instanciada com os diretorios do mapa e de onde queremos guardar,
+        o método .execute() transforma esse mapa em um GraphBounds*/
         GraphBounds graph = new OSMImporterImpl(osmFile, graphastMonacoDir).execute();
 
+        /*caso queira importar pontos de interesse para o grafo, podemos chamar o método abaixo passando
+         o grafo e o diretório do arquivo contendo os pontos de interesse(o arquivo precisa ser .scv)*/
         POIImporter.importPoIList(graph, "src/main/resources/monaco-latest.csv");
 
+        /*para gerar os custos do grafo, basta utilizar o método abaixo passando o grafo como parâmetro*/
         CostGenerator.generateAllSyntheticEdgesCosts(graph);
 
         return graph;
--- src/main/java/QueryExample.java
+++ src/main/java/QueryExample.java
@@ -10,10 +10,16 @@ public class QueryExample {
 
     public static void main(String[] agrs) {
 
+    	/*instanciando o grafo pelo método criando na classe GenerateGraphExample
+    	criada anteriormente no exemplo*/
         Graph graph = new GenerateGraphExample().monacoExample();
 
+        /*instanciando um serviço, no caso de menor caminho, mais precisamente o Dijkstra de pesos constantes.
+        basta passar o grafo como parâmetro para omstanciar o serviço*/
         AbstractShortestPathService service = new DijkstraConstantWeight(graph);
 
+        /*nesta parte são escolhidos dois nós do grafo de acordo com as coordenadas passadas,
+        então chama-se o método para encontrar o menor caminho entre os dois*/
         Long source = graph.getNodeId(43.740174,7.424376);
         Long target = graph.getNodeId(43.735554,7.416147);
         Path shortestPath = service.shortestPath(source, target);
